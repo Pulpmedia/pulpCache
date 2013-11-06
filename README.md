00a20105da6a762b83891a14a53aa1db145d89a2
Alexandre Kilian <alexandre@pulpmedia.at>
2013-11-06 10:45:16 +0100

    ADD: Documentation, Comments

--- README.md
+++ README.md
@@ -1,4 +1,70 @@
-pulpCache
-=========
+#pulpCache
 
 A simple Cache class
+
+___
+
+
+##Usage
+
+###Initialization
+
+To initialize a new Cache, simply create a new Instance of ``PulpCache``
+
+	$cache = new pulpCache();
+	
+There are a few options you can add as an array:
+	
+	cache_dir: Target directory for the cache (default ./cache)
+	config_file: Name of the Cache Configfile (defailt config.json)
+	expire_at: TimeStamp at which the Cache will be marked as old next (will be overwritten with each "save")
+	cache_name: Name of the Current cached file
+	ttl: Time to live (in Seconds). This will be used to set "expire_at" (current time + ttl). Default: 86500 (One Day)
+	
+#####Example
+	<?php
+	
+	requite_once(dirname(__FILE)) . 'pulpcache/pulpCache.php');
+	
+	$config = new array('cache_dir' => '/path/to/cache/dir',
+						 'cache_name' => 'file_to_cache',
+						 'ttl' => 3600
+						);
+	$cache = new PulpCache($config);
+	
+
+###Caching
+
+To actually cache Content, there are 3 methods you should use:
+	
+	hasCache() : Returns true if there is cached content available
+	getCache(): Returns the cached Content
+	saveCache($content): Saves $content
+	
+#####Example
+
+	<?php
+	
+	require_once(dirname(__FILE)) . 'pulpcache/pulpCache.php');
+	
+	$config = new array('cache_dir' => '/path/to/cache/dir',
+						 'cache_name' => 'file_to_cache',
+						 'ttl' => 3600
+						);
+	$cache = new PulpCache($config);
+	
+	$content = "";
+	
+	if(!$cache -> hasCache() ){
+		
+		...
+		... Fill $content wicth Content
+		...
+		
+		$cache->saveCache($content);
+		
+	} else {
+			$content = $cache -> getCache();
+	}
+	
+	echo $content
--- pulpCache.php
+++ pulpCache.php
@@ -30,10 +30,17 @@ class pulpCache {
 		return $this -> config['cache_dir'] . "/";
 	}
 
+	/**
+	 * Checks if the Cache is Fresh
+	 * @return boolean
+	 */
 	private function isFresh() {
 		return ($this -> config['expire_at'] > time());
 	}
-
+	/**
+	 * Returns the cached Content if available
+	 * @return string
+	 */
 	public function getCache() {
 		if ($this -> hasCache()) {
 			return $this -> getCacheFile();
@@ -41,10 +48,17 @@ class pulpCache {
 		return false;
 	}
 
+	/**
+	 * Returns true if there is fresh cached Content
+	 * @return boolean
+	 */
 	public function hasCache() {
 		return $this -> isFresh() && is_file($this -> getCachePath());
 	}
-
+	/**
+	 * Deletes old cached Content and saves new Cache
+	 * @return boolean
+	 */
 	public function saveCache($data) {
 		if ($this -> getCacheFile()) {
 			unlink($this -> getCachePath());
@@ -53,12 +67,21 @@ class pulpCache {
 		$this -> config['expire_at'] = time() + $this -> config['ttl'];
 		$this -> save($this -> getCachePath(), $data);
 		$this -> saveConfig();
+		return true;
 	}
 
+	/**
+	 * Returns the Directory of the cached File
+	 * @return string
+	 */
 	private function getCachePath() {
 		return $this -> getDir() . $this -> config['cache_file'];
 	}
-
+	
+	/**
+	 * Returns the content of the Cached File
+	 * @return string
+	 */
 	private function getCacheFile() {
 		$file = $this -> getCachePath();
 		if (!is_file($file)) {
@@ -67,15 +90,26 @@ class pulpCache {
 		return $this -> load($file);
 	}
 
+	/**
+	 * Creates the cache Directory
+	 */
 	public function createDir() {
 		mkdir($this -> getDir());
 	}
 
 	/*** CONFIG ***/
+	
+	/**
+	 * Creates the Cache Config File
+	 */
 	private function createConfigFile() {
 		$this -> save($this -> getConfigFile(), "");
 	}
 
+	/**
+	 * Returns the Config for the current Cache Name
+	 * @return array;
+	 */
 	private function getConfig() {
 		if (!is_file($this -> getConfigFile()))
 			return false;
@@ -101,14 +135,25 @@ class pulpCache {
 	private function getConfigFile() {
 		return $this -> getDir() . $this -> config["config_file"];
 	}
-
+	
+	/** UTILITIES **/
+	
+	 
+	/**
+	 * Loads a File
+	 * @return string
+	 */
 	private function load($file) {
-		//TODO: Check if file_get_contents is available, else use fopen
+		//TODO: Check if file_get_contents is available, else use fopen or other
 		return file_get_contents($file);
 	}
 
+	/**
+	 * Saves a File
+	 * @return boolean
+	 */
 	private function save($filename, $data) {
-		//TODO: Check if file_put_contents is available, else use fopen
+		//TODO: Check if file_put_contents is available, else use fopen or other
 		return file_put_contents($filename, $data);
 	}
 }
